f9ca5fcab8c9bef8a2ea294bafd135a85ff71ffb
muhammedunall <74102873+muhammedunall@users.noreply.github.com>
2020-12-22 17:36:11 +0300

    Add files via upload

---
+++ GlobalAIHubPythonHomework.py
@@ -0,0 +1,13 @@
+a = input("1. değişkeni girin : ")
+b = input("2. değişkeni girin : ")
+c = input("3. değişkeni girin : ")
+d = int(input("4. değişkeni girin (integer deger): "))
+e = int(input("5. değişkeni girin (integer deger): "))
+
+print("\n\n")
+
+print(f'1. değişkenin değeri {a} , Tipi {type(a)}')
+print(f'2. değişkenin değeri {b} , Tipi {type(b)}')
+print(f'3. değişkenin değeri {c} , Tipi {type(c)}')
+print("4. Değişken : {} , Değeri : {}".format(e,type(d)))
+print("5. Değişken : {} , Değeri : {}".format(e,type(e)))
